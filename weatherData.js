c3a039b1e231e4d4601a961f4828afbf5e3ee91e
NasreenKhalid <nasreen.1@live.com>
2022-02-17 15:11:09 +0300

    Initial Commit

---
+++ weatherData.js
@@ -0,0 +1,23 @@
+const request = require('request')
+const constants = require('./config')
+
+const weatherData = (address, callback) => {
+    const url  = constants.openWeatherMap.BASE_URL + encodeURIComponent(address) + `&appid=` + constants.openWeatherMap.SECRET_KEY
+
+// console.log(url)
+request({url,json:true},(error,{body})=>{
+    // console.log(body)
+    if(error){
+        callback(`Can't fetch the data`,undefined)
+    } else {
+        callback(undefined, {
+           temperature: body.main.temp,
+           description: body.weather[0].description,
+           cityName:body.name,
+           humidity:body.main.humidity
+        })
+    }
+})
+}
+
+module.exports = weatherData;
